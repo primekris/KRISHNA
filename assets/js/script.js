a195ff5bfdc20af5b6599c3c0e2fd021cad2e318
Krishna Parihar <studykrishnawork@gmail.com>
2025-07-14 20:21:24 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,143 @@
+'use strict';
+
+
+
+/**
+ * Add event listener on multiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+}
+
+
+
+/**
+ * MOBILE NAVBAR TOGGLER
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+
+const toggleNav = () => {
+  navbar.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNav);
+
+
+
+/**
+ * HEADER ANIMATION
+ * When scrolled donw to 100px header will be active
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+
+
+
+/**
+ * SLIDER
+ */
+
+const slider = document.querySelector("[data-slider]");
+const sliderContainer = document.querySelector("[data-slider-container]");
+const sliderPrevBtn = document.querySelector("[data-slider-prev]");
+const sliderNextBtn = document.querySelector("[data-slider-next]");
+
+let totalSliderVisibleItems = Number(getComputedStyle(slider).getPropertyValue("--slider-items"));
+let totalSlidableItems = sliderContainer.childElementCount - totalSliderVisibleItems;
+
+let currentSlidePos = 0;
+
+const moveSliderItem = function () {
+  sliderContainer.style.transform = `translateX(-${sliderContainer.children[currentSlidePos].offsetLeft}px)`;
+}
+
+/**
+ * NEXT SLIDE
+ */
+
+const slideNext = function () {
+  const slideEnd = currentSlidePos >= totalSlidableItems;
+
+  if (slideEnd) {
+    currentSlidePos = 0;
+  } else {
+    currentSlidePos++;
+  }
+
+  moveSliderItem();
+}
+
+sliderNextBtn.addEventListener("click", slideNext);
+
+/**
+ * PREVIOUS SLIDE
+ */
+
+const slidePrev = function () {
+  if (currentSlidePos <= 0) {
+    currentSlidePos = totalSlidableItems;
+  } else {
+    currentSlidePos--;
+  }
+
+  moveSliderItem();
+}
+
+sliderPrevBtn.addEventListener("click", slidePrev);
+
+/**
+ * RESPONSIVE
+ */
+window.addEventListener("resize", function () {
+  totalSliderVisibleItems = Number(getComputedStyle(slider).getPropertyValue("--slider-items"));
+  totalSlidableItems = sliderContainer.childElementCount - totalSliderVisibleItems;
+
+  moveSliderItem();
+});
+
+
+const themeToggleBtn = document.querySelector("[data-theme-btn]");
+const currentTheme = localStorage.getItem("theme");
+
+// Set theme on load
+if (currentTheme === "light_theme") {
+  document.body.classList.add("light_theme");
+  themeToggleBtn.classList.add("active");
+} else {
+  document.body.classList.add("dark_theme");
+  themeToggleBtn.classList.remove("active");
+}
+
+// Toggle theme on click
+themeToggleBtn.addEventListener("click", function () {
+  themeToggleBtn.classList.toggle("active");
+
+  if (themeToggleBtn.classList.contains("active")) {
+    document.body.classList.remove("dark_theme");
+    document.body.classList.add("light_theme");
+    localStorage.setItem("theme", "light_theme");
+  } else {
+    document.body.classList.remove("light_theme");
+    document.body.classList.add("dark_theme");
+    localStorage.setItem("theme", "dark_theme");
+  }
+});
+
+
